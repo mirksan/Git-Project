149cfc20720d80e79c6b45b87489fa760bd0f0e3
mirksan <mirksan1@gmail.com>
2023-11-24 11:05:12 +0900

    회원가입 추가 완료

--- src/GitMain.java
+++ src/GitMain.java
@@ -3,15 +3,15 @@ public class GitMain {
 	
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!");
+		// 회원가입 기능 추가 완료
 	}
 	
 	
 	public void join() {
-		//회원가입
-		//회원가입 수정
+		///회원가입 수정
 	}
 	
 	public void login() {
-		// 로그인
+		// 로그인 기능
 	}
 }
