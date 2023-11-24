ddefb6efdef8318e67bdd412e7ad41bf569fa8d9
mirksan <mirksan1@gmail.com>
2023-11-24 11:06:44 +0900

    로그인 기능 추가 완료

--- src/GitMain.java
+++ src/GitMain.java
@@ -3,11 +3,11 @@ public class GitMain {
 	
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!");
+		// 로그인 기능 추가 완료
 	}
 	
 	
 	public void join() {
-		//회원가입
 		//회원가입 수정
 	}
 	
